76b38bea53ec1079e4f33607c6519d00337cff73
Bozidar Arsic <40611211+BoKna031@users.noreply.github.com>
2023-02-05 23:08:21 +0100

    Minor calculation over rating.csv

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ util.create_table_of_unique_values(anime_df, 'Genres', ', ', 'genres')
 util.create_anime_genre_table()
 
 inappropriate_mal_ids = util.remove_inappropriate_content(anime_df, 'Genres', ', ', ['Hentai'])
-util.create_clean_rating_csv(inappropriate_mal_ids, ['watching_status', 'watched_episodes'], 'clean_rates')
+print('Total number of animes: ', anime_df.shape[0])
+#util.create_clean_rating_csv(inappropriate_mal_ids, ['watching_status', 'watched_episodes'], 'clean_rates')
 
-
-
-
+rating_df = pd.read_csv(cons.CLEAN_USER_RATINGS_CSV_PATH)
+voted_anime_num = len(rating_df['anime_id'].unique())
+print('Voted animes: ', voted_anime_num)
+user_num = len(rating_df['user_id'].unique())
+print('Users: ', user_num)
+rating_count = rating_df.shape[0]
+print('Rating count: ', rating_count)
+density = rating_count*100/(user_num*voted_anime_num)
+print('Matrix is', 'sparse' if density < 50 else 'dense', 'with density of ', round(density, 2), '%')
